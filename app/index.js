a3b34813693d5c22f5e653aa287fe532b30281a8
atsuhikoMochizuki <atsuhiko_mochizuki27@laposte.net>
2023-05-09 07:18:12 +0200

    MAJ

--- app/index.js
+++ app/index.js
@@ -6,6 +6,61 @@ const API_GET_REGIONS = "https://geo.api.gouv.fr/regions";
 let regions = document.getElementById("regions");
 let departments = document.getElementById("departments");
 let communes = document.getElementById("communes");
+let communeInformations = document.getElementById("communeInformations");
+
+//Affectation des callbacks aux signaux
+regions.addEventListener("change", (event) => {
+  let request =
+    "https://geo.api.gouv.fr/regions/" + regions.value + "/departements";
+
+  fetch(request)
+    .then((res) => {
+      return res.json();
+    })
+    .then((datas) => {
+      for (let data of datas) {
+        let option = document.createElement("option");
+        option.textContent = data.nom;
+        option.value = data.code;
+        departments.appendChild(option);
+      }
+    });
+});
+
+departments.addEventListener("change", (event) => {
+  let request =
+    "https://geo.api.gouv.fr/departements/" + departments.value + "/communes";
+
+  fetch(request)
+    .then((res) => {
+      return res.json();
+    })
+    .then((datas) => {
+      for (let data of datas) {
+        let option = document.createElement("option");
+        option.textContent = data.nom;
+        option.value = data.code;
+        communes.appendChild(option);
+      }
+    });
+});
+
+communes.addEventListener("change", (event) => {
+  let request = "https://geo.api.gouv.fr/communes/" + communes.value;
+
+  fetch(request)
+    .then((res) => {
+      return res.json();
+    })
+    .then((datas) => {
+      let datasToDisplay = `
+      ${datas.nom}
+      Population : ${datas.population}
+      Code postal : ${datas.code}
+      `;
+      communeInformations.value = datasToDisplay;
+    });
+});
 
 on_region_changed(regions, departments, communes);
 
@@ -32,10 +87,8 @@ function on_region_changed(regionsElem, departmentsElem, communesElem) {
       for (let data of datas) {
         let option = document.createElement("option");
         option.textContent = data.nom;
-        option.value = data.nom;
+        option.value = data.code;
         regions.appendChild(option);
       }
     });
 }
-
-regions.addEventListener("change", (event) => {});
